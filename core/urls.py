e1ddd837f5a6d8bd1c7b2f62442d970eb012f528
joel2982 <jjoel9958@gmail.com>
2024-05-28 21:57:55 +0530

    'changes'

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from core import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.index, name = 'index'),
     path('add_product', views.add_product, name = 'add_product'),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
